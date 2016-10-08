024fc195b1a30c456e5760e77246db2158bd1c38
Rcorvo <rcu1987@gmail.com>
2016-10-08 15:00:51 +0200

    Fin ejercicio 2.12

---
+++ app/src/main/java/net/example/fragmenttabhost/FragmentPestanya3.java
@@ -0,0 +1,32 @@
+package net.example.fragmenttabhost;
+
+import android.support.v4.app.Fragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Raul Corvo on 08/10/2016.
+ */
+
+public class FragmentPestanya3 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View v = inflater.inflate(R.layout.pestanya3_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        Resources res = getResources();
+        tv.setText(res.getString(R.string.eti_tab3));
+
+        return v;
+    }
+}
